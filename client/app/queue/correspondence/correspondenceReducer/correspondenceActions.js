5c96657379b3c157d0aa4e180269c153163fa5b5
Ryan Marshall <rnmarshall1993@gmail.com>
2023-11-07 09:08:37 -0800

    Added redux store to hold checkbox state.

--- client/app/queue/correspondence/correspondenceReducer/correspondenceActions.js
+++ client/app/queue/correspondence/correspondenceReducer/correspondenceActions.js
@@ -20,40 +20,12 @@ export const loadCorrespondences = (correspondences) =>
     });
   };
 
-export const loadVeteranInformation = (veteranInformation) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.LOAD_VETERAN_INFORMATION,
-      payload: {
-        veteranInformation
-      }
-    });
-  };
-
-export const loadVetCorrespondence = (vetCorrespondences) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.LOAD_VET_CORRESPONDENCE,
-      payload: {
-        vetCorrespondences
-      }
-    });
-  };
-
-export const updateRadioValue = (value) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.UPDATE_RADIO_VALUE,
-      payload: value
-    });
-  };
-
-export const saveCheckboxState = (correspondence, isChecked) =>
+export const saveCheckboxState = (id, isChecked) =>
   (dispatch) => {
     dispatch({
       type: ACTIONS.SAVE_CHECKBOX_STATE,
       payload: {
-        correspondence, isChecked
+        id, isChecked
       }
     });
   };
@@ -64,63 +36,3 @@ export const clearCheckboxState = () =>
       type: ACTIONS.CLEAR_CHECKBOX_STATE,
     });
   };
-
-export const setTaskRelatedAppealIds = (appealIds) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.SET_TASK_RELATED_APPEAL_IDS,
-      payload: {
-        appealIds
-      }
-    });
-  };
-
-export const setUnrelatedTasks = (tasks) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.SET_UNRELATED_TASKS,
-      payload: {
-        tasks
-      }
-    });
-  };
-
-export const setFetchedAppeals = (appeals) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.SET_FETCHED_APPEALS,
-      payload: {
-        appeals
-      }
-    });
-  };
-
-export const saveMailTaskState = (name, isChecked) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.SAVE_MAIL_TASK_STATE,
-      payload: {
-        name,
-        isChecked
-      }
-    });
-  };
-
-export const setNewAppealRelatedTasks = (newAppealRelatedTasks) =>
-  (dispatch) => {
-    dispatch({
-      type: ACTIONS.SET_NEW_APPEAL_RELATED_TASKS,
-      payload: {
-        newAppealRelatedTasks
-      }
-    });
-  };
-
-export const setWaivedEvidenceTasks = (task) => (dispatch) => {
-  dispatch({
-    type: ACTIONS.SET_WAIVED_EVIDENCE_TASKS,
-    payload: {
-      task
-    }
-  });
-};
--- client/app/queue/correspondence/correspondenceReducer/correspondenceReducer.js
+++ client/app/queue/correspondence/correspondenceReducer/correspondenceReducer.js
@@ -2,17 +2,8 @@ import { update } from '../../../util/ReducerUtil';
 import { ACTIONS } from './correspondenceConstants';
 
 export const initialState = {
-  taskRelatedAppealIds: [],
-  newAppealRelatedTasks: [],
-  fetchedAppeals: [],
   correspondences: [],
-  radioValue: '0',
-  relatedCorrespondences: [],
-  mailTasks: {},
-  unrelatedTasks: [],
-  currentCorrespondence: [],
-  veteranInformation: [],
-  waivedEvidenceTasks: [],
+  toggledCheckboxes: []
 };
 
 export const intakeCorrespondenceReducer = (state = initialState, action = {}) => {
@@ -31,90 +22,25 @@ export const intakeCorrespondenceReducer = (state = initialState, action = {}) =
       }
     });
 
-  case ACTIONS.LOAD_VETERAN_INFORMATION:
-    return update(state, {
-      veteranInformation: {
-        $set: action.payload.veteranInformation
-      }
-    });
-
-  case ACTIONS.LOAD_VET_CORRESPONDENCE:
-    return update(state, {
-      vetCorrespondences: {
-        $set: action.payload.vetCorrespondences
-      }
-    });
-
-  case ACTIONS.UPDATE_RADIO_VALUE:
-    return update(state, {
-      radioValue: {
-        $set: action.payload.radioValue
-      }
-    });
-
   case ACTIONS.SAVE_CHECKBOX_STATE:
     if (action.payload.isChecked) {
       return update(state, {
-        relatedCorrespondences: {
-          $push: [action.payload.correspondence]
+        toggledCheckboxes: {
+          $push: [action.payload.id]
         }
       });
     }
 
     return update(state, {
-      relatedCorrespondences: {
-        $set: state.relatedCorrespondences.filter((corr) => corr.id !== action.payload.correspondence.id)
+      toggledCheckboxes: {
+        $set: state.toggledCheckboxes.filter((id) => id !== action.payload.id)
       }
     });
 
   case ACTIONS.CLEAR_CHECKBOX_STATE:
     return update(state, {
-      relatedCorrespondences: {
+      toggledCheckboxes: {
         $set: []
-      }
-    });
-
-  case ACTIONS.SET_UNRELATED_TASKS:
-    return update(state, {
-      unrelatedTasks: {
-        $set: [...action.payload.tasks]
-      }
-    });
-
-  case ACTIONS.SET_FETCHED_APPEALS:
-    return update(state, {
-      fetchedAppeals: {
-        $set: [...action.payload.appeals]
-      }
-    });
-
-  case ACTIONS.SAVE_MAIL_TASK_STATE:
-    return update(state, {
-      mailTasks: {
-        [action.payload.name]: {
-          $set: action.payload.isChecked
-        }
-      }
-    });
-
-  case ACTIONS.SET_TASK_RELATED_APPEAL_IDS:
-    return update(state, {
-      taskRelatedAppealIds: {
-        $set: [...action.payload.appealIds]
-      }
-    });
-
-  case ACTIONS.SET_NEW_APPEAL_RELATED_TASKS:
-    return update(state, {
-      newAppealRelatedTasks: {
-        $set: [...action.payload.newAppealRelatedTasks]
-      }
-    });
-
-  case ACTIONS.SET_WAIVED_EVIDENCE_TASKS:
-    return update(state, {
-      waivedEvidenceTasks: {
-        $set: [...action.payload.task]
       }
     });
 
