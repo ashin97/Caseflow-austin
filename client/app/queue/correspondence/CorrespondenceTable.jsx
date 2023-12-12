69cff6fc14e7e32d258c2f4ed5a07b8e9bb7f757
Kevin Ma <magavakevin10@gmail.com>
2023-12-12 15:39:46 -0500

    Fixed filtering bug, refactored correspondenceTable to be functional component

--- client/app/queue/correspondence/CorrespondenceTable.jsx
+++ client/app/queue/correspondence/CorrespondenceTable.jsx
@@ -3,58 +3,59 @@ import QueueTable from '../QueueTable';
 import PropTypes from 'prop-types';
 import Link from '@department-of-veterans-affairs/caseflow-frontend-toolkit/components/Link';
 
-class CorrespondenceTable extends React.Component {
+const CorrespondenceTable = (props) => {
 
-  render() {
+  const columns = [
+    {
+      name: 'veteranDetails',
+      header: 'Veteran Details',
+      align: 'left',
+      valueName: 'veteranDetails',
+      getSortValue: (row) => row.firstName,
+      backendCanSort: true,
+      valueFunction: (row) => (
+        <Link href={`/queue/correspondence/${row.correspondenceUuid}/review_package`}>
+          {`${row.firstName} ${row.lastName} (${row.fileNumber})`}
+        </Link>
+      )
+    },
 
-    const columns = [
-      {
-        name: 'veteranDetails',
-        header: 'Veteran Details',
-        align: 'left',
-        valueName: 'veteranDetails',
-        getSortValue: (row) => row.firstName,
-        backendCanSort: true,
-        valueFunction: (row) => (
-          <Link href={`/queue/correspondence/${row.correspondenceUuid}/review_package`}>
-            {`${row.firstName} ${row.lastName} (${row.fileNumber})`}
-          </Link>
-        )
+    {
+      name: 'packageDocumentType',
+      header: 'Package Document Type ',
+      align: 'left',
+      columnName: 'packageDocumentType',
+      valueName: 'packageDocumentType',
+      enableFilter: true,
+      anyFiltersAreSet: true,
+      tableData: props.vetCorrespondences,
+      label: 'Filter by package document type',
+      getSortValue: (row) => row.packageDocumentType,
+      filterValueTransform: (columnValue) => {
+        return String(columnValue);
       },
-      {
-        name: 'packageDocumentType',
-        header: 'Package Document Type ',
-        align: 'left',
-        valueName: 'packageDocumentType',
-        enableFilter: true,
-        getSortValue: (row) => row.packageDocumentType,
-        backendCanSort: true
-      },
-      {
-        name: 'cmPacketNumber',
-        header: 'CM Packet Number ',
-        align: 'left',
-        valueName: 'cmPacketNumber',
-        getSortValue: (row) => row.cmPacketNumber,
-        backendCanSort: true
-      }
-    ];
-    const tabPaginationOptions = {
-      onPageLoaded: this.onPageLoaded
-    };
+      backendCanSort: true
+    },
+    {
+      name: 'cmPacketNumber',
+      header: 'CM Packet Number ',
+      align: 'left',
+      valueName: 'cmPacketNumber',
+      getSortValue: (row) => row.cmPacketNumber,
+      backendCanSort: true
+    }
+  ];
 
-    return (
-      <QueueTable
-        className="assign-correspondence-table"
-        columns={columns}
-        rowObjects={this.props.vetCorrespondences}
-        summary="scheduled-hearings-table"
-        enablePagination
-        tabPaginationOptions={tabPaginationOptions}
-      />
-    );
-  }
-}
+  return (
+    <QueueTable
+      className="assign-correspondence-table"
+      columns={columns}
+      rowObjects={props.vetCorrespondences}
+      summary="scheduled-hearings-table"
+      enablePagination
+    />
+  );
+};
 
 CorrespondenceTable.propTypes = {
   hearingScheduleColumns: PropTypes.array,
