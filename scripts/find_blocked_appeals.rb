7d5f04ccb56d5e4d63b98869bdafbca672eb22f5
Craig Reese <Reese_Craig@bah.com>
2024-02-22 14:16:49 -0600

    move script to scripts folder

--- scripts/find_blocked_appeals.rb
+++ scripts/find_blocked_appeals.rb
@@ -4,7 +4,7 @@
 # appeal.can_redistribute_appeal? which prevents the appeal from being distributed
 #
 # To run from caseflow root directory:
-#    bundle exec rails runner lib/helper/find_blocked_appeals.rb arg1 arg2 arg3
+#    bundle exec rails runner scripts/find_blocked_appeals.rb arg1 arg2 arg3
 #
 # Required arguments:
 #   priority: true or false; if no blocking appeals found, provide the other boolean
@@ -13,17 +13,17 @@
 # Optional arguments:
 #   treee: whether or not to print the appeal "treee" method output to the console
 
-if ARGV[1].blank? || ARGV[2].blank?
+if ARGV[0].blank? || ARGV[1].blank?
   fail "Required arguments: priority and number of appeals"
 end
 
-fail "priority must be true or false" unless %w[true false].include?(ARGV[1])
-fail "number of appeals must be a non-zero integer" unless ARGV[2].to_i > 0
-fail "treee must be true or false" unless ARGV[3].blank? || %w[true false].include?(ARGV[3])
+fail "priority must be true or false" unless %w[true false].include?(ARGV[0])
+fail "number of appeals must be a non-zero integer" unless ARGV[1].to_i > 0
+fail "treee must be true or false" unless ARGV[2].blank? || %w[true false].include?(ARGV[2])
 
-priority = ARGV[1]
-number_of_appeals = ARGV[2].to_i
-treee = ARGV[3].presence ? ARGV[3] : false
+priority = ARGV[0]
+number_of_appeals = ARGV[1].to_i
+treee = ARGV[2].presence ? ARGV[2] : false
 
 # silence ActiveRecord logging
 ActiveRecord::Base.logger = nil
